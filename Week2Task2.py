c3b8085f9454a14b4607b09486289b0a5b7d0192
1206Priyanka <43996054+1206Priyanka@users.noreply.github.com>
2020-03-14 13:50:05 +0000

    Add files via upload

---
+++ Week2Task2.py
@@ -0,0 +1,9 @@
+
+#setting the variable radius to the integer value the user inputs
+radius = int(input("What is the radius of the circle? \n"))
+
+#function that calculates the area of the circle
+circleArea = radius*radius*3.14
+
+#prints out the value of the radius
+print(circleArea)
